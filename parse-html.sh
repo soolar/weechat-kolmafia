9f0f8f2b1c257e96f3be6dd152f79e25fabc5a6b
Joseph Kichline <soolar121@gmail.com>
2016-10-14 16:05:50 -0700

    Strip out background color codes from parsed html
    It was always setting the background color to 0 for some reason, which I
    didn't want, so I looked in to how to parse ANSI color codes and got rid
    of it.

--- parse-html.sh
+++ parse-html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo $1- | elinks -dump -dump-color-mode 3 -no-numbering -no-references -eval 'set document.dump.width = 300' | sed -r 's/[[:space:]]*$//; s/[[:space:]]{3}//'
+echo $1- | elinks -dump -dump-color-mode 3 -no-numbering -no-references -eval 'set document.dump.width = 300' -eval 'set terminal.screen-xterm-256color.transparency = 1' | sed -r 's/[[:space:]]*$//; s/[[:space:]]{3}//; /\-$/ d; s/\o33\[48;5;0m//g'
 
