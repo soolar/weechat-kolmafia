7c744875dd7df41d6349917b6a0e9757d4d98b9c
Joseph Kichline <soolar121@gmail.com>
2016-10-14 18:18:31 -0700

    Fix empty line deletion
    The switch to 16 color mode made it not match the regex I had used, so I
    improved the regex to apply to any and all possible chains of SGR
    parameters.

--- parse-html.sh
+++ parse-html.sh
@@ -6,5 +6,5 @@ sed -r 's/[[:space:]]*$//' | # strip off trailing whitespace
 sed -r 's/[[:space:]]{3}//' | # strip off the three spaces elinks likes to add to the start of the line
 sed -r 's/\o33\[48;5;0m//g' | # remove color codes that set the background to 0 since elinks just will not obey the transparency setting
 sed -r 's/-$//' | # If there's a hyphen at the end of the line, ditch it. There always seem to be random floating hyphens coming out of nowhere
-sed -r '/^(\o33\[38;5;[[:digit:]]+m)+$/ d' # get rid of lines that are just color codes
+sed -r '/^(\o33\[[[:digit:]]*(;[[:digit:]]+)+m)+$/ d' # get rid of lines that are just color codes
 
